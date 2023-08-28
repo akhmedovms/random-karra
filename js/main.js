fbb58d53bdcd4c67288ec2e2a25cfafaa036daf7
mukhammad_sadiy <akhmedovms@gmail.com>
2023-08-28 11:43:45 +0500

    Add favicon and change site title

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
-const elLoader = document.querySelector('.js-loader');
+const elLoader = document.querySelector(".js-loader");
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener("DOMContentLoaded", () => {
   setTimeout(() => {
-    elLoader.classList.add('loader-wrapper--none');
+    elLoader.classList.add("loader-wrapper--none");
   }, 800);
 });
